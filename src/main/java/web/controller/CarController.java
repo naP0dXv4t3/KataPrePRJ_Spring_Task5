aedf6a54dcd1067fe0fffec52a2b37bbc04193ef
naP0dXv4t3 <smokewithcheese@gmail.com>
2024-04-16 17:52:39 +0300

    ...

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,32 +1,35 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.CarService.CarService;
 import web.model.Car;
+import web.service.CarService;
+import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-public class CarController {
+public class CarController  {
 
     @GetMapping("/cars")
     public String cars (@RequestParam(value = "count", defaultValue = "5") int allCars, Model car) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("Ford", "Mustang", "1950"));
-        cars.add(new Car("Ferrari", "Enzo", "1976"));
-        cars.add(new Car("BMW", "520i", "2016"));
-        cars.add(new Car("Hyundai", "i40", "2012"));
-        cars.add(new Car("Volkswagen", "Tiguan", "2019"));
-        cars = CarService.carsCount(cars, allCars);
-        car.addAttribute("cars",cars);
+        CarServiceImpl carServiceImpl = new CarServiceImpl();
+        car.addAttribute("cars",  carServiceImpl.getAllCars());
+        car.addAttribute("cars", carServiceImpl.carsCount(carServiceImpl.cars, allCars));
         return "cars";
+        }
+
+
+
 
 
 
     }
 
-}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,27 @@
+package web.service;
+
+import org.springframework.stereotype.Component;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class CarServiceImpl implements CarService{
+    @Override
+    public List<Car> carsCount(List<Car> cars, int count){
+        return cars.stream().limit(count).toList();
+    }
+
+
+    public List<Car> cars = new ArrayList<>();
+
+    public List<Car> getAllCars() {
+        cars.add(new Car("Ford", "Mustang", "1950"));
+        cars.add(new Car("Ferrari", "Enzo", "1976"));
+        cars.add(new Car("BMW", "520i", "2016"));
+        cars.add(new Car("Hyundai", "i40", "2012"));
+        cars.add(new Car("Volkswagen", "Tiguan", "2019"));
+        return  cars;
+}
+}
+
