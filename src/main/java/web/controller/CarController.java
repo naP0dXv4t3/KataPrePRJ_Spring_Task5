c2cdfd611d4242776a05c2888546ace9fb6a80c4
naP0dXv4t3 <smokewithcheese@gmail.com>
2024-04-17 01:19:31 +0300

    ...

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,19 +1,13 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController  {
